be9423235e8ff95f0eed7140a63a274239b6abaa
Guilherme Curcio <guilherme.crcio@gmail.com>
2017-10-09 13:34:06 -0300

    commit

--- app/controllers/sessao_controller.rb
+++ app/controllers/sessao_controller.rb
@@ -4,43 +4,55 @@ class SessaoController < ApplicationController
   
   before_action :getRequestBody  
   attr_accessor :usuario
+  attr_accessor :erros
+  
+  def initialize
+    super
+    @erros = Array.new
+  end
   
   def web
-    login do
-      @usuario.update_attribute :token_web, Digest::SHA256.hexdigest(@usuario["email"]+(Time.now.to_s))
+    begin
+      login do
+        @usuario.update_attribute :token_web, Digest::SHA256.hexdigest(@usuario["email"]+(Time.now.to_s))
+      end
+      
+      @usuario.delete "token_mobile"
+      @usuario.delete "token_expiracao_mobile"
+      
+      render json: { resultado: @usuario }, status: 201
+    rescue => e
+      render json: { erros: @erros }, status: 400
     end
-    
-    @usuario.delete "token_mobile"
-    @usuario.delete "token_expiracao_mobile"
-    
-    render json: { resultado: @usuario }, status: 201
   end
   
   def mobile
-    login do
-      @usuario.update_attribute :token_mobile, Digest::SHA256.hexdigest(@usuario["email"]+(Time.now.to_s))
-      @usuario.update_attribute :token_expiracao_mobile, (Time.now + (72 * 60 * 60))
+    begin
+      login do
+        @usuario.update_attribute :token_mobile, Digest::SHA256.hexdigest(@usuario["email"]+(Time.now.to_s))
+        @usuario.update_attribute :token_expiracao_mobile, (Time.now + (72 * 60 * 60))
+      end
+      
+      @usuario.delete "token_web"
+      
+      render json: { resultado: @usuario }, status: 201
+    rescue => e
+      render json: { erros: @erros }, status: 400
     end
-    
-    @usuario.delete "token_web"
-    
-    render json: { resultado: @usuario }, status: 201
   end
   
   private
   def login
-    erros = Array.new
-    
     if @data["email"].nil?
-      erros << "Informe o Email"
+      @erros << "Informe o Email"
     end
     
     if @data["senha"].nil?
-      erros << "Informe a Senha"
+      @erros << "Informe a Senha"
     end
     
-    if erros.length > 0
-      return render json: { erros: erros }, status: 400
+    if @erros.length > 0
+      raise true
     end
     
     @usuario = Usuario.find_by(
@@ -48,14 +60,9 @@ class SessaoController < ApplicationController
       senha: (Digest::SHA256.hexdigest @data["senha"])
     )
     
-    if @usuario.blank?
-      erros << "Usuário inválido"
-      return render json: { erros: erros }, status: 400
-    end
-    
-    if @usuario.blank?
-      erros << "Usuário inválido"
-      return render json: { erros: erros }, status: 400
+    if @usuario.nil?
+      @erros << "Usuário inválido"
+      raise true
     end
     
     yield
@@ -64,8 +71,8 @@ class SessaoController < ApplicationController
       @usuario = @usuario.as_json
       @usuario.delete "senha"
     else
-      erros << "Erro ao efetuar login"
-      render json: { erros: erros }, status: 400
+      @erros << "Erro ao efetuar login"
+      raise true
     end
   end
   
